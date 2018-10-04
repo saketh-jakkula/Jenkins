0cf4c72774a0f8c1798df66ef35914e5e55ff2e2
saketh-linux <sakethjakkula642@gmail.com>
2018-10-04 13:08:53 +0530

    Jenkins installion

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,35 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+execute 'apt update' do
+  command 'apt-get update'
+end
+
+remote_file '/tmp/jenkins-ci.org.key' do
+  source 'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
+  notifies :run, 'execute[apt key add]', :immediately
+end
+
+execute 'apt key add' do
+  command 'apt-key add /tmp/jenkins-ci.org.key'
+  action :nothing
+end
+
+file '/etc/apt/sources.list.d/jenkins.list' do
+  content 'deb http://pkg.jenkins.io/debian-stable binary/'
+  notifies :run, 'execute[apt update]', :immediately
+end
+
+execute 'apt update' do
+  command 'apt-get update'
+  action :nothing
+end
+
+package 'openjdk-8-jre-headless'
+
+package 'jenkins'
+
+service 'jenkins' do
+  action :start
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,8 +9,6 @@ require 'spec_helper'
 describe 'jenkins::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
@@ -18,18 +16,38 @@ describe 'jenkins::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'updates apt' do
+	expect(chef_run).to run_execute 'apt-get update'
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'downloads the key' do
+	expect(chef_run).to create_remote_file '/tmp/jenkins-ci.org.key'
     end
-  end
+
+    it 'execute apt-key add' do
+	expect(chef_run.remote_file('/tmp/jenkins-ci.org.key')).to notify('execute[apt key add]').to(:run).immediately
+    end
+
+    it 'edits the jenkins.list' do
+	expect(chef_run).to create_file '/etc/apt/sources.list.d/jenkins.list'
+    end
+  
+    it 'updates apt' do
+	expect(chef_run.file('/etc/apt/sources.list.d/jenkins.list')).to notify('execute[apt update]').to(:run).immediately
+    end
+
+    it 'installs java' do
+	expect(chef_run).to install_package 'openjdk-8-jre-headless'
+    end
+
+    it 'installs jenkins' do
+	expect(chef_run).to install_package 'jenkins'
+    end
+
+    it 'start jenkins' do
+	expect(chef_run).to start_service 'jenkins'
+    end
+end
+
 end
